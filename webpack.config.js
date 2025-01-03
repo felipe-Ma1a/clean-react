b1c2b650aaa55d03fad667a0bffbf2cee1926a5f
Felipe Maia <felipemaia850@gmail.com>
2025-01-02 22:43:48 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { rules } = require("eslint-plugin-react");
 
 module.exports = {
   mode: "development",
@@ -10,10 +11,36 @@ module.exports = {
     filename: "bundle.js",
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", "scss"],
     alias: {
       "@": path.join(__dirname, "src"),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     contentBase: "./public",
